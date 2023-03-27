f76254c8293bf017de3e08353d78aa34a97e24c9
corno <corno.astn.a@schraverus.com>
2023-03-27 13:44:42 +0200

    .

--- typescript/pub/src/api.generated.ts
+++ typescript/pub/src/api.generated.ts
@@ -3,6 +3,14 @@ import * as pt from 'pareto-core-types'
 import * as g_common from "glo-pareto-common"
 import * as g_this from "./glossary"
 
+export namespace D {
+    
+    
+    
+    
+    
+}
+
 export namespace A {
     
     export type createErrorLogger = () => g_this.ASYNC.A.C.CreateStringLogger
